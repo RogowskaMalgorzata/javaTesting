6f3d1325647818910ec03538efdff35e984f1f69
Malgorzata Rogowska <21gos12@gmail.com>
2016-03-08 11:33:47 +0100

    finished zad2

--- lab01/src/test/java/zad2/CalculatorTest.java
+++ lab01/src/test/java/zad2/CalculatorTest.java
@@ -1,6 +1,6 @@
 package zad2;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 import org.junit.Test;
 
@@ -10,9 +10,27 @@ public class CalculatorTest {
 	Calculator calc = new Calculator();
 	
 	@Test
-	public void checkAdd(){
-		 assertEquals(2.2, calc.add(1.1, 1.1), 0);
+	public void checkSub(){
+		 assertEquals(2.2, calc.sub(3.3, 1.1), 0.05);
 	}
 	
+	@Test
+	public void checkAdd() {
+		assertEquals(2.2, calc.add(1.1256, 1.1111), 0.05);
+	}
 	
-} 
+	@Test
+	public void checkDiv() {
+		assertEquals(2, calc.div(4.4, 2), 0.5);
+	}
+	
+	@Test
+	public void checkMulti() {
+		assertEquals(6.6, calc.multi(2.2, 3), 0.05);
+	}
+	
+	@Test
+	public void checkGreater() {
+		assertFalse(calc.greater(3.4, 5));
+	}
+ } 
